a1a129075ff6fd5adb77325d5daacd3786374442
fullerm-BMCHS <120735545+fullerm-BMCHS@users.noreply.github.com>
2023-04-23 16:41:28 -0400

    Working on new version of bell schedule

---
+++ script.js
@@ -0,0 +1,251 @@
+// Data for Bell Schedules and Calendar
+var data = { 
+    "btn-MF" : [
+        ["Period","Start","End","Duration","Passing"],        
+        [" HR", " 7:40 am", " 7:46 am", "0:06", "0:06"],
+        ["1st", " 7:52 am", " 8:39 am", "0:47", "0:06"],
+        ["2nd", " 8:45 am", " 9:32 am", "0:47", "0:06"],
+        ["3rd", " 9:38 am", "10:25 am", "0:47", "0:06"],
+        ["4th", "10:31 am", "11:18 am", "0:47", "0:06"],
+        ["5th", "11:24 am", "12:11 pm", "0:47", "0:06"],
+        ["6th", "12:17 pm", " 1:04 pm", "0:47", "0:06"],
+        ["7th", " 1:10 pm", " 1:57 pm", "0:47", "0:06"],
+        ["8th", " 2:03 pm", " 2:50 pm", "0:47", "0:06"]],
+    "btn-Wed" : [
+        ["Period","Start","End","Duration","Passing"],
+        [" HR", " 7:40 am", " 7:48 am", "0:08", "0:06"],
+        ["1st", " 7:54 am", " 8:37 am", "0:43", "0:06"],
+        ["2nd", " 8:43 am", " 9:26 am", "0:43", "0:06"],
+        ["3rd", " 9:32 am", "10:15 am", "0:43", "0:06"],
+        ["4th", "10:21 am", "11:04 am", "0:43", "0:06"],
+        ["5th", "11:10 am", "11:53 am", "0:43", "0:06"],
+        ["6th", "11:59 am", "12:42 pm", "0:43", "0:06"],
+        ["7th", "12:48 pm", " 1:31 pm", "0:43", "0:06"],
+        ["8th", " 1:37 pm", " 2:20 pm", "0:43", "0:06"]],
+    "btn-Mass" : [
+        ["Period","Start","End","Duration","Passing"],
+        [" HR", " 7:40 am", " 7:45 am", "0:05", "0:06"],
+        ["1st", " 7:51 am", " 8:33 am", "0:42", "0:06"],
+        ["2nd", " 8:39 am", " 9:21 am", "0:42", "0:06"],
+        ["To HR", " 9:27 am", " 9:32 am", "0:05", "0:06"],
+        ["3rd", " 9:38 am", "10:25 am", "0:47", "0:06"],
+        ["7th", "10:31 am", "11:18 am", "0:47", "0:06"],
+        ["4th", "11:24 am", "12:11 pm", "0:47", "0:06"],
+        ["5th", "12:17 pm", " 1:04 pm", "0:47", "0:06"],
+        ["6th", " 1:10 pm", " 1:57 pm", "0:47", "0:06"],
+        ["8th", " 2:03 pm", " 2:50 pm", "0:47", "0:06"]],
+    "btn-1pm" : [
+        ["Period","Start","End","Duration","Passing"],
+        [" HR", " 7:40 am", " 7:48 am", "0:08", "0:06"],
+        ["1st", " 7:54 am", " 8:27 am", "0:33", "0:06"],
+        ["2nd", " 8:33 am", " 9:06 am", "0:33", "0:06"],
+        ["3rd", " 9:12 am", " 9:45 am", "0:33", "0:06"],
+        ["4th", " 9:51 am", "10:24 am", "0:33", "0:06"],
+        ["5th", "10:30 am", "11:03 am", "0:33", "0:06"],
+        ["6th", "11:09 am", "11:42 am", "0:33", "0:06"],
+        ["7th", "11:48 am", "12:21 pm", "0:33", "0:06"],
+        ["8th", "12:27 pm", " 1:00 pm", "0:33", "0:06"]],
+    "btn-d001" : [
+        ["Period","Start","End","Duration","Passing"],
+        [" HR", " 7:40 am", " 7:46 am", "0:06", "0:06"],
+        ["1st", " 7:52 am", " 8:39 am", "0:47", "0:06"],
+        ["2nd", " 8:45 am", " 9:32 am", "0:47", "0:06"],
+        ["3rd", " 9:38 am", "10:25 am", "0:47", "0:06"],
+        ["4th", "10:31 am", "11:18 am", "0:47", "0:06"],
+        ["5th", "11:24 am", "12:11 pm", "0:47", "0:06"],
+        ["6th", "12:17 pm", " 1:04 pm", "0:47", "0:06"],
+        ["7th", " 1:10 pm", " 1:57 pm", "0:47", "0:06"],
+        ["8th", " 2:03 pm", " 2:50 pm", "0:47", "0:06"]],
+    "btn-Cal" : [
+        ["Date","Day","Event"],
+        ["04/24/2023", "Monday", "Spring Art Show 5pm - 7pm"],
+        ["04/25/2023", "Tuesday", "Junior Class Mass <br> Shrek the Musical 7:30pm - 9:00pm"],
+        ["04/26/2023", "Wednesday", "Shrek the Musical 7:30pm - 9:00pm"],
+        ["04/27/2023", "Thursday", "Shrek the Musical 7:30pm - 9:00pm"],
+        ["04/28/2023", "Friday", "Grad Nite 3:00pm - 12:00am"],
+        ["05/01/2023", "Monday", "AP Testing:<br>8am-United States Government and Politics <br>12pm-Chemistry and Spanish Literature and Culture<br>11:59pm Deadline for AP Computer Science Principles students to submit their Create performance task as final."],
+        ["05/02/2023", "Tuesday", "AP Testing:<br>8am-Environmental Science <br>12pm-Psychology"],
+        ["05/03/2023", "Wednesday", "AP Testing:<br>8am-English Literature and Composition <br>12pm-Comparative Government and Politics<br>12pm-Computer Science A"],
+        ["05/04/2023", "Thursday", "AP Testing:<br>8am-Human Geography<br>8am-Macroeconomics<br>12pm-Statistics"],
+        ["05/05/2023", "Friday", "AP Testing:<br>8am-European History<br>8am-United States History<br>12pm-Microeconomics"],
+        ["05/08/2023", "Monday", "AP Testing:<br>8am-Calculus AB<br>8am-Calculus BC<br>12pm-Computer Science Principles<br>12pm-Italian Language and Culture"],
+        ["05/09/2023", "Tuesday", "AP Testing:<br>8am-English Language and Composition<br>12pm-Computer Science Principles<br>12pm-Physics C"],
+        ["05/10/2023", "Wednesday", "AP Testing:<br>8am-Spanish Language and Culture<br>12pm-Biology"],
+        ["05/11/2023", "Thursday", "AP Testing:<br>8am-French Language and Culture<br>8am-World History: Modern<br>12pm-Physics 1: Algebra-Based"],
+        ["05/12/2023", "Friday", "AP Testing:<br>8am-German Language and Culture<br>8am-Music Theory<br>12pm-Latin<br>12pm-Physics 2: Algebra-Based"],
+        ["05/13/2023", "Saturday", "Hornet Expo (previously known as Freshmen Fair) 7:45am - 11:45am in Dome"],
+        ["05/15/2023", "Monday", "Awards Ceremony 6:30pm - 8pm"],
+        ["05/17/2023", "Wednesday", "Senior Yearbook Distribution in Dome during lunch periods"],
+        ["05/18/2023", "Thursday", "Underclassmen Yearbook Distribution in Dome during religion class"],
+        ["05/19/2023", "Friday", "Senior Grades Due"],
+        ["05/25/2023", "Thursday", "No School - Baccalaureate and Graduation"],
+        ["05/29/2023", "Monday", "No School - Memorial Day Holiday"],
+        ["05/30/2023", "Tuesday", "Semester Exams:<br>Period 3: 8:00am - 9:30am<br>Period 6: 10:00am - 11:30am"],
+        ["05/31/2023", "Wednesday", "Semester Exams:<br>Period 7: 8:00am - 9:30am<br>Period 4: 10:00am - 11:30am"],
+        ["06/01/2023", "Thursday", "Semester Exams:<br>Period 1: 8:00am - 9:30am<br>Period 8: 10:00am - 11:30am"],
+        ["06/02/2023", "Friday", "Semester Exams:<br>Period 2: 8:00am - 9:30am<br>Period 5: 10:00am - 11:30am<br>End of Semester<br>Last Day of School"],
+        ["06/05/2023", "Monday", "Faculty Post-Planning"],
+        ["06/06/2023", "Tuesday", "Faculty Post-Planning"],
+        ["06/07/2023", "Wednesday", "Summer School Begins"],
+        ["06/19/2023", "Monday", "No School - Juneteenth - Office Closed"],
+        ["06/30/2023", "Friday", "Summer School Ends"],
+        ["07/04/2023", "Tuesday", "Independence Day - Office Closed"],
+        ["08/09/2023", "Wednesday", "First Day of School"],
+        ["09/04/2023", "Monday", "No School - Labor Day"],
+        ["09/19/2023", "Tuesday", "Back to School Night"],
+        ["09/21/2023", "Thursday", "STEM Night"],
+        ["10/06/2023", "Friday",  "No School<br>Teacher PD<br>End of 1st Quarter"],
+        ["10/14/2023", "Saturday", "Homecoming Dance"],
+        ["10/20/2023", "Friday", "No School (Regional PD)"],
+        ["11/20/2023 - 11/24/2023", "Monday – Friday", "No School - Thanksgiving Break<br>Monday and Tuesday are Hurricane Makeup Days"],
+        ["12/12/2023", "Tuesday", "Semester Exams:<br>Period ?: 8:00am - 9:30am<br>Period ?: 10:00am - 11:30am"],
+        ["12/13/2023", "Wednesday", "Semester Exams:<br>Period ?: 8:00am - 9:30am<br>Period ?: 10:00am - 11:30am"],
+        ["12/14/2023", "Thursday", "Semester Exams:<br>Period ?: 8:00am - 9:30am<br>Period ?: 10:00am - 11:30am"],
+        ["12/15/2023", "Friday", "Semester Exams:<br>Period ?: 8:00am - 9:30am<br>Period ?: 10:00am - 11:30am<br>End of Quarter 2 - End of Semester 1"],
+        ["12/18/2023 - 12/29/2023", "Monday – Friday", "No School - Christmas Break"],
+        ["01/01/2024 - 01/02/2024", "Monday – Tuesday", "No School"],
+        ["01/03/2024", "Wednesday", "2nd Semester Begins"],
+        ["01/15/2024", "Monday", "No School (Martin Luther King Jr.)"],
+        ["02/04/2024", "Sunday", "Grandparents Day Celebration"],
+        ["02/16/2024", "Friday", "No School (Regional PD)"],
+        ["02/19/2024", "Monday", "No School (Presidents’ Day)"],
+        ["03/08/2024", "Friday", "End of 3rd Quarter"],
+        ["03/18/2024 - 03/22/2024", "Monday – Friday", "No School - Spring Break"],
+        ["03/28/2024 - 04/01/2024", "Thursday – Monday", "No School - Easter Break"],
+        ["05/13/2024", "Monday", "Awards Ceremony"],
+        ["05/23/2024", "Thursday", "No School - Baccalaureate Mass & Graduation"],
+        ["05/27/2024", "Monday", "No School (Memorial Day)"],
+        ["05/28/2024", "Tuesday", "Semester Exams:<br>Period ?: 8:00am - 9:30am<br>Period ?: 10:00am - 11:30am"],
+        ["05/29/2024", "Wednesday", "Semester Exams:<br>Period ?: 8:00am - 9:30am<br>Period ?: 10:00am - 11:30am"],
+        ["05/30/2024", "Thursday", "Semester Exams:<br>Period ?: 8:00am - 9:30am<br>Period ?: 10:00am - 11:30am"],
+        ["05/31/2024", "Friday", "Semester Exams:<br>Period ?: 8:00am - 9:30am<br>Period ?: 10:00am - 11:30am<br>End of Semester"],
+    ],                            
+}
+
+// Global State Variable to know if Calendar is being shown
+//     Need to disable period highlighting
+currentButton = "";
+
+// Respond to topnav menu button clicked 
+function navClick(btnClicked) {
+    // Change active button to button clicked
+    activeButtons = document.getElementsByClassName("active");
+    activeButtons[0].classList.remove("active"); //Only 1 item should be returned
+    document.getElementById(btnClicked).classList.add("active");
+    
+    // Build table corresponding to button id clicked
+    currentButton = btnClicked;
+    buildTable(data[btnClicked]);
+    setInterval(updateScreen, 1000);
+}
+
+// Generic build table from data array
+function buildTable(tableArr) {
+    // Remove existing schedule
+    schedule = document.getElementById("schedule");
+    schedule.innerHTML = "";
+    const [headings, ...rows] = tableArr;
+    tableString = '<table id="schedTable"><thead>';
+    for (let header of headings) {
+        tableString += "<th>" + header + "</th>";
+    }
+    tableString += "</thead><tbody>"
+
+    for (let aRow of rows) {
+        tableString += "<tr>";
+        for (let cell of aRow) {
+            tableString += "<td>" + cell + "</td>";
+        }
+        tableString += "</tr>";
+    }
+    tableString += "</tbody></table>";
+    schedule.innerHTML = tableString;
+}
+
+// Helper function for updateScreen()
+function getDate(timeStr) {
+    let now = new Date();
+    let hr = parseInt(timeStr.split(":")[0]);
+    let mi = parseInt((timeStr.split(":")[1]).split(" ")[0]);
+    let meridiem = timeStr.split(" ")[1];
+    if (meridiem == "pm" && hr < 12) {
+        hr = hr + 12;
+    } 
+    now.setHours(hr, mi, 0, 0);
+    return now;
+}
+
+// Updates Screen every second for clock and highlighting the period
+function updateScreen() {
+    // Get current time - need seconds
+    let now = new Date();
+
+    // Update Clock
+    const timeDiv = document.getElementById("time");
+    const dateString = now.toLocaleString('en-US');
+    timeDiv.innerHTML = "<h3>CLOCK: " + dateString + "</h3>";
+
+    if (currentButton == "btn-Cal")
+        return;
+        
+    // Get table to highlight current period
+    let table = document.getElementById("schedTable");
+
+    // Highlight Current Period
+    for (var r = 1, n = table.rows.length; r < n; r++) {
+        let startStr = table.rows[r].cells[1].innerHTML;
+        let startTime = getDate(startStr)
+        let endStr = table.rows[r].cells[2].innerHTML;
+        let endTime = getDate(endStr);
+        // console.log(startTime, now, endTime);
+        if (startTime <= now && now <= endTime) {
+            // console.log("Row:", r);
+            table.rows[r].cells[0].style.color = 'red';
+            table.rows[r].cells[0].style.backgroundColor = 'Cornsilk';
+            table.rows[r].cells[1].style.color = 'red';
+            table.rows[r].cells[1].style.backgroundColor = 'Cornsilk';
+            table.rows[r].cells[2].style.color = 'red';
+            table.rows[r].cells[2].style.backgroundColor = 'Cornsilk';
+            table.rows[r].cells[3].style.color = 'red';
+            table.rows[r].cells[3].style.backgroundColor = 'Cornsilk';
+            table.rows[r].cells[4].style.color = 'black';
+            table.rows[r].cells[4].style.backgroundColor = 'aqua';
+            if (r > 1) {
+                table.rows[r-1].cells[4].style.color = 'black';
+                table.rows[r-1].cells[4].style.backgroundColor = 'aqua'; 
+            }
+        }
+    }
+    // Highlight if in passing between periods
+    for (var r = 1, n = table.rows.length; r < n-1; r++) {
+        let startStr = table.rows[r].cells[2].innerHTML;
+        let startTime = getDate(startStr)
+        let endStr = table.rows[r+1].cells[1].innerHTML;
+        let endTime = getDate(endStr);
+        // console.log(startTime, now, endTime);
+        if (startTime <= now && now <= endTime) {
+            // console.log("Row:", r);
+            table.rows[r].cells[4].style.color = 'red';
+            table.rows[r].cells[4].style.backgroundColor = 'Cornsilk';
+            table.rows[r].cells[3].style.color = 'black';
+            table.rows[r].cells[3].style.backgroundColor = 'aqua';
+            table.rows[r].cells[2].style.color = 'black';
+            table.rows[r].cells[2].style.backgroundColor = 'aqua';
+            table.rows[r].cells[1].style.color = 'black';
+            table.rows[r].cells[1].style.backgroundColor = 'aqua';
+            table.rows[r].cells[0].style.color = 'black';
+            table.rows[r].cells[0].style.backgroundColor = 'aqua';
+        }
+    }
+}
+
+// Determine default button on startup
+let day = new Date();
+
+if (day.toLocaleDateString('en-Us') == "4/21/2023")
+    document.getElementById("btn-d001").click();
+else if (day.getDay() == 3)
+    document.getElementById("btn-Wed").click();
+else
+    document.getElementById("btn-MF").click();
+

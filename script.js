9ed0bcb288bf3ad0af669aebb2b2964fa38a44a5
fullerm-BMCHS <120735545+fullerm-BMCHS@users.noreply.github.com>
2024-01-29 12:30:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -417,7 +417,7 @@ if (day.toLocaleDateString('en-Us') == "8/9/2023")
 else if (day.toLocaleDateString('en-Us') == "1/9/2024")
     document.getElementById("btn-Wed").click();
 else if (day.toLocaleDateString('en-Us') == "1/29/2024")
-    document.getElementById("btn-allMass").click();
+    document.getElementById("btn-campMass").click();
 else if (day.toLocaleDateString('en-Us') == "2/14/2024")
     document.getElementById("btn-allMass").click();
 else if (day.toLocaleDateString('en-Us') == "3/7/2024")

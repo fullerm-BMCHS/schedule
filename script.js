ed9eb6a992faf4cf7eebcd91eae4f3a6442d3513
fullerm-BMCHS <120735545+fullerm-BMCHS@users.noreply.github.com>
2024-02-29 15:46:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -150,9 +150,9 @@ var data = {
     "btn-Cal" : [
         ["Date","Day","Event"],
         ["02/29/2024", "Thursday", "Lenten Penance Service (During Lunches)"],
-        ["03/01/2024", "Friday", "Pep Rally"],
+        ["03/01/2024", "Friday", "<br>Fire Drill<br>Period 1-Dorsey, Dome, Moore Center<br>Period 2-Loretta, MM, Gym<br>Period 3-Bishop Borders/ Bishop Grady / Cafeteria"],
         ["03/01/2024", "Friday", "Letters of Intent Due"],
-        ["03/06/2024", "Wednesday", "Noon Dismissal<br>Fire Drill<br>Period 1-Dorsey, Dome, Moore Center<br>Period 2-Loretta, MM, Gym<br>Period 3-Bishop Borders/ Bishop Grady / Cafeteria"],
+        ["03/06/2024", "Wednesday", "Noon Dismissal"],
         ["03/07/2024", "Thursday", "Senior Class Mass"],
         ["03/08/2024", "Friday", "No School for Students<br>End of 3rd Quarter"],
         ["03/12/2024", "Tuesday", "ACT (Juniors)"],
@@ -447,8 +447,8 @@ if (day.toLocaleDateString('en-Us') == "8/9/2023")
     document.getElementById("btn-wBless").click();
 else if (day.toLocaleDateString('en-Us') == "2/23/2024")
     document.getElementById("btn-d001").click();
-else if (day.toLocaleDateString('en-Us') == "3/1/2024")
-    document.getElementById("btn-pepAM").click();
+else if (day.toLocaleDateString('en-Us') == "3/6/2024")
+    document.getElementById("btn-noon").click();
 else if (day.toLocaleDateString('en-Us') == "3/7/2024")
     document.getElementById("btn-classMass").click();
 else if (day.toLocaleDateString('en-Us') == "3/14/2024")

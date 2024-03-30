01529e4985b2d21c597e87eb5c165cdbe4c71ceb
Miko Jimenez <jimenezmikog@gmail.com>
2024-03-30 13:30:49 -0400

    Continue refining CSS
    Minor adjustments to navbar

--- scripts/stylings.js
+++ scripts/stylings.js
@@ -2,12 +2,7 @@
 // Overlayes stylings while minimally impacting original implementation/
 ////////////////////////////////////////////////////////////////////////
 
-var dateButtons = 'button[id^=btn-]';
-
-// Center button items for mobile collapsed navbar
-$(dateButtons).addClass('w-100');
-
 // Close navbar on click for collabed navbar
-$(dateButtons).on('click', function(){
+$('button[id^=btn-]').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });

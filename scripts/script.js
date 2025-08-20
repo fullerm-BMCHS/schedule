72d5e95731cfeb8b0d4387e9385d9a3a9fe532d5
fullerm-BMCHS <120735545+fullerm-BMCHS@users.noreply.github.com>
2025-08-20 10:23:00 -0400

    Update script.js calendar updates

--- scripts/script.js
+++ scripts/script.js
@@ -154,17 +154,18 @@ var data = {
     "btn-Cal" : [
         ["Date","Day","Event"],
         ["2024-2025", "BMCHS", "<a href='https://bishopmoore.gofmx.com/calendar' target='_blank'>FMX Calendar</a>"],
-        ["08/18/2025", "Monday", "Dept Chair Meeting"],
         ["08/20/2025", "Wednesday", "Fire Drill: <a href='https://docs.google.com/spreadsheets/d/1s_k__cjW58Oxgj6wjTB9dAeJeAruWP8B65aJ6Ly10SQ/edit?gid=0#gid=0'>Teacher Lineup</a><br>1st Period: BD, Dome, MC<br>2nd Period: BB, BG, Cafe<br>3rd Period: LO,MM,Gym<br>9th hour - Department Meeting"],
-        ["08/21/2025", "Thursday", "Lunch n'Learn<br>Back to School Night 6:00 pm"],
+        ["08/21/2025", "Thursday", "Back to School Night 6:00 pm<a href='https://docs.google.com/document/d/1qnAOc2n82QBf70cUSdDY49EjInxZihIAX98tTo00iig/edit?usp=sharing'>Schedule</a>"],
         ["08/25/2025", "Monday", "Lockdown Drill / Lockout Drill"],
-        ["08/26/2025", "Tuesday", "Weather Hold Drill"],
+        ["08/26/2025", "Tuesday", "Weather Hold Drill<br>Lunch n'Learn: Formative Assesments"],
         ["08/27/2025", "Wednesday", "9th hour - New Teacher PLC / Faithfit Bookclub / Individual PD"],
         ["08/28/2025", "Thursday", "Freshman Retreat (Day 1)"],
         ["08/29/2025", "Friday", "Freshman Retreat (Day 2)"],
         ["09/01/2025", "Monday", "NO SCHOOL - Labor Day"],
+        ["09/02/2025", "Tuesday", "Cutoff for textbook purchases. eTextbook assignments can be given"],
         ["09/03/2025", "Wednesday", "9th hour - Faculty Meeting"],
         ["09/09/2025", "Tuesday", "Lunch n'Learn - Incorporating Catholic Identity in the classroom"],
+        ["09/10/2025", "Wednesday", "9th hour - New"],
         ["09/11/2025", "Thursday", "All School Mass (10am - 11am)"],
         ["09/12/2025", "Friday", "Progress Reports Q1"],
         ["09/15/2025", "Monday", "Dept Chair Meeting"],
@@ -175,24 +176,24 @@ var data = {
         ["10/01/2025", "Wednesday", "Herf Jones Assembly Seniors (2nd), Juniors (3rd)<br>9th hour - Faculty Meeting"],
         ["10/02/2025", "Thursday", "Faculty and Staff Yearbook Picture Retake Day (Pines 7:30am - 9:30am"],
         ["10/04/2025 - 10/11/2025", "Saturday - Saturday", "Fall Mission Trip to the Dominican Republic (25 Seniors)"],
-        ["10/07/2025", "Tuesday", "Lunch n'Lean - Instructional Strategies"],
+        ["10/07/2025", "Tuesday", "Lunch n'Learn - Instructional Strategies"],
         ["10/08/2025", "Wednesday", "PSAT 9th Grade Testing<br>9th hour - Individual PD"],
         ["10/09/2025", "Thursday", "PSAT 10th Grade Testing"],
         ["10/10/2025", "Friday", "PSAT 11th Grade Testing<br>End of Q1"],
         ["10/13/2025", "Monday", "NO SCHOOL - Teacher Work Day"],
         ["10/15/2025", "Wednesday", "9th hour - Common-Course PLC"],
         ["10/16/2025", "Thursday", "All School Mass (10am - 11am)"],
-        ["10/15/2025", "Wednesday", "9th hour - Department PLC"],
+        ["10/22/2025", "Wednesday", "9th hour - Department PLC"],
         ["10/25/2025", "Saturday", "Homecoming Dance"],
         ["10/29/2025", "Wednesday", "9th hour - New Teacher PLC / Faithfit Bookclub / Individual PD"],
-        ["10/30/2025", "Thursday", "Lunch n'Lean - Ed Tech"],
+        ["10/30/2025", "Thursday", "Lunch n'Learn - Ed Tech"],
         ["10/31/2025", "Friday", "Sophomore Retreat (Day 1)"],
         ["11/02/2025", "Sunday", "Open House"],
         ["11/03/2025", "Monday", "Sophomore Retreat (Day 2)"],
         ["11/05/2025", "Wednesday", "9th hour - Faculty Meeting"],
         ["11/11/2025", "Tuesday", "All School Mass (10am - 11am)"],
         ["11/12/2025", "Wednesday", "9th hour - New Teacher PLC / Individual PD"],
-        ["11/13/2025", "Thursday", "Lunch n'Lean - TBD"],
+        ["11/13/2025", "Thursday", "Lunch n'Learn - TBD"],
         ["11/14/2025", "Friday", "Progress Reports Q2"],
         ["11/17/2025", "Monday", "Dept Chair Meeting"],
         ["11/11/2025", "Tuesday", "Fire Drill During Lunch"],
@@ -237,6 +238,7 @@ var data = {
         ["08/13/2025", "Wednesday", "9th hour - Faculty Meeting"],
         ["08/14/2025", "Thursday", "Yearbook Picture Day - Freshman, Sophomore, Junior"],
         ["08/15/2025", "Friday", "All School Mass (10am - 11am) <a href='https://docs.google.com/spreadsheets/d/1U8yRWXlBcS17uDNW3NEOwu9mjgZSxZcog_szhU4kmTY/edit?gid=171656829#gid=171656829'>Seating Chart</a><br>Back-to-School Slides due to evaluator<br>New Teacher Check-in Meeting (Optional)"],
+        ["08/18/2025", "Monday", "Dept Chair Meeting"],
     ]                
 }
 
@@ -374,6 +376,7 @@ else if (day.getDay() == 3)
     document.getElementById("btn-Wed").click();
 else
     document.getElementById("btn-MF").click();
+
 
 
 

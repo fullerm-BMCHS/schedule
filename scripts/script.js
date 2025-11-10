a546cc5da5491cce03cf7e9f0ea97d72f697357d
fullerm-BMCHS <120735545+fullerm-BMCHS@users.noreply.github.com>
2025-11-09 19:41:45 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -155,14 +155,10 @@ var data = {
     "btn-Cal" : [
         ["Date","Day","Event"],
         ["2024-2025", "BMCHS", "<a href='https://bishopmoore.gofmx.com/calendar' target='_blank'>FMX Calendar</a>"],
-        ["10/31/2025", "Friday", "Sophomore Retreat (Day 1)"],
-        ["11/02/2025", "Sunday", "Open House 1pm to 3pm"],
-        ["11/03/2025", "Monday", "Sophomore Retreat (Day 2)"],
-        ["11/05/2025", "Wednesday", "9th hour - Faculty Meeting"],
         ["11/10/2025", "Monday", "Fire Drill During Lunch<br>Cafeteria Only<br>Alarm will sound in multiple buildings"],
         ["11/11/2025", "Tuesday", "All School Mass (10am - 11am)"],
         ["11/12/2025", "Wednesday", "9th hour - New Teacher PLC / Individual PD"],
-        ["11/13/2025", "Thursday", "Lunch n'Learn - TBD"],
+        ["11/13/2025", "Thursday", "Lunch n'Learn - Using Technology to Collect Formative Data"],
         ["11/14/2025", "Friday", "Progress Reports Q2"],
         ["11/17/2025", "Monday", "Dept Chair Meeting"],
         ["11/11/2025", "Tuesday", "Fire Drill During Lunch"],
@@ -172,7 +168,7 @@ var data = {
         ["12/03/2025", "Wednesday", "9th hour - Faculty Meeting"],
         ["12/05/2025", "Friday", "Email to parents of students with D/F grades"],
         ["12/08/2025", "Monday", "All School Mass (10am - 11am)"],
-        ["12/10/2025", "Wednesday", "ASP Exams Due to Testing Center<br>9th hour - Individual PD"],
+        ["12/10/2025", "Wednesday", "ASP Exams Due to Testing Center<br>9th hour - Common Course PLC"],
         ["12/12/2025", "Friday", "End of Quarter 2"],
         ["12/16/2025", "Tuesday", "Semester Exams:<br>Period 1: 8:00am - 9:30am<br>Period 2: 10:00am - 11:30am"],
         ["12/17/2025", "Wednesday", "Semester Exams:<br>Period 3: 8:00am - 9:30am<br>Period 4: 10:00am - 11:30am"],
@@ -249,6 +245,10 @@ var data = {
         ["10/27/2025", "Monday", "Fire Education Video (3rd Period - 4 minutes)"],
         ["10/29/2025", "Wednesday", "9th hour - New Teacher PLC / Faithfit Bookclub / Individual PD"],
         ["10/30/2025", "Thursday", "Lunch n'Learn - Ed Tech"],
+        ["10/31/2025", "Friday", "Sophomore Retreat (Day 1)"],
+        ["11/02/2025", "Sunday", "Open House 1pm to 3pm"],
+        ["11/03/2025", "Monday", "Sophomore Retreat (Day 2)"],
+        ["11/05/2025", "Wednesday", "9th hour - Faculty Meeting"],
     ]                
 }
 
@@ -390,6 +390,7 @@ else if (day.getDay() == 3)
     document.getElementById("btn-Wed").click();
 else
     document.getElementById("btn-MF").click();
+
 
 
 
